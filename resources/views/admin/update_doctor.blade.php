a80c92fa0cd154277551b9a173e9558dc02605ff
kipkemoilvs <kipkemoilvs@gmail.com>
2023-05-12 11:58:11 +0300

    ft: update doctor styling

--- resources/views/admin/update_doctor.blade.php
+++ resources/views/admin/update_doctor.blade.php
@@ -10,9 +10,27 @@
         @include('admin.sidebar')
         @include('admin.navbar')
         <div class="container-fluid page-body-wrapper">
-            {{$data->name}}
-            {{$data->phone}}
-            {{$data->room}}
+            <div class="container" align="center">
+                <form action="">
+                    <div style="padding: 15px">
+                        <label for="">doctor's name</label>
+                        <input style="color: black" type="text" name="name" value="{{$data->name}}">
+                    </div>
+                    <div style="padding: 15px">
+                        <label for="">doctor's phone</label>
+                        <input style="color: black" type="number" name="name" value="{{$data->phone}}">
+                    </div>
+                    <div style="padding: 15px">
+                        <label for="">doctor's speciality</label>
+                        <input style="color: black"type="text" name="name" value="{{$data->speciality}}">
+                    </div>
+                    <div style="padding: 15px">
+                        <label for="">doctor's room</label>
+                        <input style="color: black"type="text" name="name" value="{{$data->room}}">
+                    </div>
+                </form>
+
+            </div>
         </div>
     </div>
     @include('admin.script')
